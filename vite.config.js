be8f86d89610cb14882900a6a9809a6e83e9d8c9
XasanovBilol <dadabaevolim7@gmail.com>
2026-01-21 23:15:13 +0500

    Initial commit: Vue + Vite source code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base: '/zizi_./',
+    base: '/zizi-dev/',
     plugins: [
         vue(),
     ],
